6976ba932120edb8cd2eb5eeb5303b6eecfbfb61
Sashini Shihara <sashinisr98@gmail.com>
2021-10-13 20:47:01 +0530

    Events

---
+++ app/src/main/java/com/mydev/reminderapplication/AddEvent2.java
@@ -0,0 +1,14 @@
+package com.mydev.reminderapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEvent2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event2);
+    }
+}
